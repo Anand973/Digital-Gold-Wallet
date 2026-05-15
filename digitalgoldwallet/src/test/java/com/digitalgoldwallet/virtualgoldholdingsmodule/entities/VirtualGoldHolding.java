366881ec697d03fac32a65b89a47910bd6fc07af
SwaritSharma <swarit.developer@gmail.com>
2026-05-15 12:17:35 +0530

    Virtual Gold Holding Entity

---
+++ digitalgoldwallet/src/test/java/com/digitalgoldwallet/virtualgoldholdingsmodule/entities/VirtualGoldHolding.java
@@ -0,0 +1,54 @@
+package com.digitalgoldwallet.virtualgoldholdingsmodule.entities;
+
+import com.digitalgoldwallet.usermodule.entities.User;
+import com.digitalgoldwallet.vendormodule.entities.VendorBranch;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "virtual_gold_holdings")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class VirtualGoldHolding {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "holding_id")
+    private Integer holdingId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "branch_id", nullable = false)
+    private VendorBranch branch;
+
+    @NotNull(message = "Quantity cannot be null")
+    @DecimalMin(
+            value = "0.01",
+            message = "Quantity must be greater than 0"
+    )
+    @Column(
+            nullable = false,
+            precision = 18,
+            scale = 2
+    )
+    private BigDecimal quantity;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void setCreatedAt() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
